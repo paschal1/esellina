c053a5faddcd58de2eeca1548e0a5c4177e126f8
starite paschal <paschalnwokeocha@gmail.com>
2021-09-18 12:49:38 +0100

    big
    boat

--- esellina/pschat/pdoconfig.php
+++ esellina/pschat/pdoconfig.php
@@ -4,8 +4,8 @@ $dbname = 'epspixhw_esellina';
 $username = 'epspixhw_starite';
 $password = 'paschal@081';
 
-$host = 'localhost';
-$dbname = 'esellina';
-$username = 'root';
-$password = '';
+// $host = 'localhost';
+// $dbname = 'esellina';
+// $username = 'root';
+// $password = '';
 ?>
