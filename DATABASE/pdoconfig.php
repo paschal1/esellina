fcafdda0bb5bf984bc1b0cbb72f4eb4d44b29454
starite paschal <paschalnwokeocha@gmail.com>
2022-05-24 13:42:39 +0100

    ok

--- DATABASE/pdoconfig.php
+++ DATABASE/pdoconfig.php
@@ -1,11 +1,12 @@
 <?php
-// $host = 'localhost';
-// $dbname = 'esellina';
-// $username = 'root';
-// $password = '';
 
 $host = 'localhost';
 $dbname = 'eseltwgh_esellina';
 $username = 'eseltwgh_starite';
 $password = 'paschal@081';
+
+// $host = 'localhost';
+// $dbname = 'esellina';
+// $username = 'root';
+// $password = '';
 ?>
