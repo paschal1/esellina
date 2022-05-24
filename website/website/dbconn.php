c3cdad5f1f4cdc3abc2488ddcb343ab1adfb9d48
starite paschal <paschalnwokeocha@gmail.com>
2022-05-24 13:59:46 +0100

    ok

--- website/website/dbconn.php
+++ website/website/dbconn.php
@@ -1,16 +1,5 @@
 <?php
-$dbconn = mysqli_connect("localhost","root","","folder");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-
-  date_default_timezone_set("Africa/lagos"); 
-
-
-// $dbconn = mysqli_connect("localhost","epspixhw_starite","paschal@081","epspixhw_esellina");
+// $dbconn = mysqli_connect("localhost","root","","folder");
 
 // // Check connection
 // if (mysqli_connect_errno())
@@ -19,5 +8,16 @@ if (mysqli_connect_errno())
 //   }
 
 //   date_default_timezone_set("Africa/lagos"); 
+
+
+$dbconn = mysqli_connect("localhost","epspixhw_starite","paschal@081","epspixhw_esellina");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  date_default_timezone_set("Africa/lagos"); 
 
 ?>
